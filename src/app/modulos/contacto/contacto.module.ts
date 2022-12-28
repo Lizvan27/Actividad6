db0024b604999da1f79ae8f89ce78345cf03aa5f
Lizvan27 <lizvanmandy@gmail.com>
2022-12-28 14:47:30 -0500

    agregando componentes

--- src/app/modulos/contacto/contacto.module.ts
+++ src/app/modulos/contacto/contacto.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComponetsComponent } from './componets/componets.component';
+import { PagesComponent } from './pages/pages.component';
+import { ComponentsComponent } from './components/components.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ComponetsComponent,
+    PagesComponent,
+    ComponentsComponent
+  ],
   imports: [
     CommonModule
   ]
